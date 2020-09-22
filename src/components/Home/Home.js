782b672797c646ba06d9c7407333e24827bbe151
Eli Reiners <goelyukim@gmail.com>
2020-09-22 15:55:18 -0400

    Initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+    <>
+                <nav className="homeNav">Meeting app</nav>
+                <section className="homeBody">
+                    <Link to='/new-meeting'>Create meeting</Link>
+                    <Link to='/meetings'>Your meetings</Link>
+                    <Link to='/trends'>Team trends</Link>
+            </section>
+       </>
+        )  
+    }
+}
