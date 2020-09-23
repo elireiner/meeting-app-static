bb0c9047df5ef5bc3698c16f651f60873ef5ae49
Eli Reiners <goelyukim@gmail.com>
2020-09-23 16:36:51 -0400

    Add link to meeting page

--- src/components/MeetingLink/MeetingLink.js
+++ src/components/MeetingLink/MeetingLink.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class MeetingLink extends React.Component{
-    render(){
-        return(
+export default class MeetingLink extends React.Component {
+    render() {
+        return (
             <>
-           <li>{this.props.name}</li>
+                <Link to={`meeting/${this.props._id}`}>{this.props.name}</Link>
             </>
         )
     }
