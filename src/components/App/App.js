fc1d8321e06eb9500d60428628f33ded19de4443
Eli Reiners <goelyukim@gmail.com>
2020-09-23 16:15:02 -0400

    Add support for Home routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Home from '../Home/Home';
+import NewMeeting from '../NewMeeting/NewMeeting';
+import Meetings from '../Meetings/Meetings';
+import Teams from '../Teams/Teams';
 
 export default class App extends React.Component {
 
   render() {
-      return (
-          <Home/>
-      )
+    return (
+
+      <div className="App">
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Home
+            />
+          )}
+        />
+
+        <Route path="/new-meeting" component={NewMeeting} />{" "}
+        <Route path="/meetings" component={Meetings} />{" "}
+        <Route path="/teams" component={Teams} />{" "}
+      </div>
+    )
   }
 }
