8ccb6292eb92c9039733c1ee23765f065643a484
Eli Reiners <goelyukim@gmail.com>
2020-09-23 16:37:00 -0400

    Initial commit

---
+++ src/components/MeetingPage/MeetingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class MeetingPage extends React.Component{
+    render(){
+        return(
+            <p>hi {//this.props.name
+            }</p>
+        )
+    }
+}
