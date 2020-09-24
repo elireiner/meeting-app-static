4eaaed1564f0c1bf719ce1dc8b61ea6e096dbaa4
Eli Reiners <goelyukim@gmail.com>
2020-09-24 08:49:27 -0400

    Add link to Assess And add meeting info

--- src/components/MeetingPage/MeetingPage.js
+++ src/components/MeetingPage/MeetingPage.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { meetings } from '../../mock-data';
 
-export default class MeetingPage extends React.Component{
-    render(){
-        return(
-            <p>hi {//this.props.name
-            }</p>
+export default class MeetingPage extends React.Component {
+    render() {
+
+        // Get meeting id
+        let meetingId;
+        if (this.props.match.params.meetingId) {
+            meetingId = this.props.match.params.meetingId;
+        }
+        else {
+            let pathname = this.props.location.pathname;
+            let pathnameMinusSlashOne = pathname.substring(pathname.indexOf("/") + 1);
+            meetingId = pathnameMinusSlashOne.substring(pathnameMinusSlashOne.indexOf("/") + 1);
+            meetingId.trim();
+        }
+
+        //Get meeting object
+        let meeting = meetings.filter(meeting => Object.values(meeting).includes(meetingId))[0]
+
+        //Set path and property for Assess link
+        const newTo = {
+            pathname: `/assess/${meetingId}`,
+            meeting: meeting
+        };
+        
+        return (
+            <>
+                <p>{meeting.timestamp}</p>
+                <p>{meeting.name}</p>
+                <p>{meeting.type}</p>
+                <p>{meeting.department}</p>
+                <p>{meeting.description}</p>
+                <Link to={newTo}>Assess</Link>
+            </>
         )
     }
 }
