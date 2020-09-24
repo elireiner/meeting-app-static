5794954686e946562fe8e4dcb5949b7cbf19d5fb
Eli Reiners <goelyukim@gmail.com>
2020-09-24 08:49:36 -0400

    Initial commit

---
+++ src/components/Assess/Assess.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class Assess extends React.Component {
+    
+    render() {
+        console.log(this.props)
+        return (
+            <>
+                <h1>How great was the meeting on these metrics?</h1>
+                <form>
+                    <label>
+                        Clarity
+                    <input type="number" />
+                    </label>
+                    <label>
+                        Listening
+                    <input type="number" />
+                    </label>
+                    <label>
+                        Shared time
+                    <input type="number" />
+                    </label>
+                    <input type="submit" />
+                </form>
+
+            </>
+        )
+    }
+}
