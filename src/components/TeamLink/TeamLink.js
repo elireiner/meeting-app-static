44031ff7c875ec4d1285577faa2065a2a07ca6f7
Eli Reiners <goelyukim@gmail.com>
2020-09-24 12:08:11 -0400

    Add link to team page

--- src/components/TeamLink/TeamLink.js
+++ src/components/TeamLink/TeamLink.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default class TeamLink extends React.Component{
-    render(){
+export default class TeamLink extends React.Component {
+    render() {
+        console.log(this.props)
+        const newTo = {
+            pathname: `team/${this.props._id}`,
+            team: this.props
+        };
         return (
-        <li>{this.props.name}</li>
+            <Link to={newTo}>{this.props.name}</Link>
         )
     }
 }
